35903a7c7f767139999a091d4e557eb938d50f42
Adrien <adrien.3100@gmail.com>
2021-10-31 19:08:18 +0100

    udape main and add config_template.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
-#include <string> 
 
-#include <config.h>
-#include <wifi_con.h>
+#include <wifi_c.h>
 #include <http_request.h>
 #include <ifttt.h>
+#include "config.h"
 
 // Prototype
-Ifttt ifttt_mail(GMAIL_TRIGGER);           // Initialize Ifttt object with Gmail trigger.
+Ifttt ifttt_mail(GMAIL_TRIGGER, IFTTT_KEY);   // Initialize Ifttt object with Gmail trigger.
+Wifi wifi(WIFI_SSID, WIFI_PWD);               // Initialize wifi object
 
 // Variables
 int sum_measure;
@@ -18,7 +18,10 @@ void setup() {
   pinMode(MOISTURE_SENSOR_PIN,INPUT);
   adcAttachPin(MOISTURE_SENSOR_PIN);
   
-  wifi_connect(&Serial);
+  if (!wifi.connect()){
+    Serial.print("ERROR: WiFI fail connection\n");
+    esp_deep_sleep_start();
+  }
   
   delay(2000); 
 }
@@ -26,22 +29,27 @@ void setup() {
 void loop() {  
   Serial.println("Reading From the Sensor ...");
   
-   
+  // Performs the measurements.
   for (int n_measure = 0; n_measure < NB_MEASURE; n_measure++){
     int soil_moisture = analogRead(MOISTURE_SENSOR_PIN);
-    Serial.print(soil_moisture);
-    Serial.print("\n");
+    Serial.println(soil_moisture);
     sum_measure += soil_moisture;
     delay(4000);  // 4s
   }
   
-  int avg_measure = sum_measure / NB_MEASURE; // calculates the average of the calibration measures
+  // Calculates the average of the calibration measures
+  int avg_measure = sum_measure / NB_MEASURE; 
+
+  // Built the mail 
   String mail_object = "Finished calibration:<br>";
   String mail_body = "The average for this type of soil is : " + String(avg_measure) + ".<br><br>ESP32 is asleep.<br><br>To start a new calibration press the EN button.<br>";
-  ifttt_mail.post_request(MAILTO, mail_object, mail_body);
+  String url = ifttt_mail.get_url();
+  String content = ifttt_mail.build_content(MAILTO, mail_object, mail_body);
+
+  // Send mail
+  post_http_request(url, content);
   
-  Serial.print("\n");
-  Serial.println("Calibration finish, going to sleep ...\n");
+  Serial.println("Calibration finish, going to sleep ...");
   
   delay(2000);
   
